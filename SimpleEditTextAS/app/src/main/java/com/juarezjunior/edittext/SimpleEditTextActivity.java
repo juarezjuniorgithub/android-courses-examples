ec6e2a1d0f1f818bee47182aa9fdb5cccd89d43f
juarezjunior <juarez.barbosa@gmail.com>
2015-05-17 22:12:26 -0300

    @juarezjuniorgithub

---
+++ SimpleEditTextAS/app/src/main/java/com/juarezjunior/edittext/SimpleEditTextActivity.java
@@ -0,0 +1,16 @@
+package com.juarezjunior.edittext;
+
+import com.juarezjunior.edittext.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleEditTextActivity extends Activity {
+  
+	@Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    setContentView(R.layout.main);
+  }
+	
+}
