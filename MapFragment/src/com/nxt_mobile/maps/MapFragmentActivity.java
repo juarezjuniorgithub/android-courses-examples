87dff23012595783539e173711dbc179dac559a4
juarezjuniorgithub <juarez.barbosa@gmail.com>
2015-08-15 15:31:42 -0300

    @juarezjuniorgithub

---
+++ MapFragment/src/com/nxt_mobile/maps/MapFragmentActivity.java
@@ -0,0 +1,13 @@
+package com.nxt_mobile.maps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapFragmentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
