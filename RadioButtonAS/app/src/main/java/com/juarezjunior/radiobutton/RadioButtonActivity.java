cb74c315a2bd5ba45cc7a8e48858571af2c9bf6d
juarezjuniorgithub <juarez.barbosa@gmail.com>
2015-08-15 17:15:13 -0300

    @juarezjuniorgithub

---
+++ RadioButtonAS/app/src/main/java/com/juarezjunior/radiobutton/RadioButtonActivity.java
@@ -0,0 +1,16 @@
+
+
+package com.juarezjunior.radiobutton;
+
+import com.nxt_mobile.radiobutton.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RadioButtonActivity extends Activity {
+  @Override
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    setContentView(R.layout.main);
+  }
+}
